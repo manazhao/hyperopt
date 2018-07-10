074ba8c179e85b391df0723198189959276d02dc
qizhao <manazhao@gmail.com>
2018-07-10 08:10:31 -0700

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+```bash
+sudo pip install hyperopt
+sudo  pip install networkx==1.11
+```
---
+++ hello.py
@@ -0,0 +1,9 @@
+from hyperopt import fmin, tpe, hp
+
+best = fmin(
+    fn=lambda x: x**2,
+    space=hp.uniform('x', -10, 10),
+    algo=tpe.suggest,
+    max_evals=1000)
+
+print(best)
