b55d4bffde431caafcb51595fe0c0874ff711b03
qizhao <manazhao@gmail.com>
2018-07-11 23:03:21 -0700

    use hp.choice for num_layers and num_nodes

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from hyperopt import fmin, tpe, hp
+import hyperopt.pyll.stochastic
 
 
 def objective_function2(args):
@@ -13,9 +14,29 @@ def objective_function2(args):
 # Space is defined by value combinations of num_layers and num_nodes. When a
 # sample is generated from this space, it will be assigned to
 # objective_function2.
-space2 = [hp.randint('num_layers', 20) - 10, hp.randint('num_nodes', 20) - 10]
+# space2 = [hp.randint('num_layers', 20) - 10, hp.randint('num_nodes', 20) - 10]
+#
+# best = fmin(
+#     fn=objective_function2, space=space2, algo=tpe.suggest, max_evals=1000)
+#
+# print(best)
+
+# nested choice:
+# https://medium.com/district-data-labs/parameter-tuning-with-hyperopt-faa86acdfdce
+
+
+def objective_function3(args):
+  num_layers = args['num_layers']
+  num_nodes = args['num_nodes']
+  return (num_layers - 5)**2 + num_nodes**2
+
+
+space3 = {
+    'num_layers': hp.choice('num_layers', [3, 4, 5]),
+    'num_nodes': hp.choice('num_nodes', [10, 50, 100])
+}
 
 best = fmin(
-    fn=objective_function2, space=space2, algo=tpe.suggest, max_evals=1000)
+    fn=objective_function3, space=space3, algo=tpe.suggest, max_evals=1000)
 
 print(best)
